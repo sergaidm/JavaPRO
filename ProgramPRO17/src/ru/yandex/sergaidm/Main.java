1b9c64658c9621568e5a3b14cda8829f793c4ef3
sergaidm <sergaidm@yandex.ru>
2018-05-21 16:28:14 +0300

    1. Annotations. Annotation for sorting objects using annotated class

--- ProgramPRO17/src/ru/yandex/sergaidm/Main.java
+++ ProgramPRO17/src/ru/yandex/sergaidm/Main.java
@@ -6,10 +6,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 
 		List<Student> students = new ArrayList<>();
@@ -23,23 +25,25 @@ public class Main {
 		students.add(new Student("Andreeva", 23, "female", "DP-51s"));
 		students.add(new Student("Sergeeva", 22.9, "female", "DP-51s"));
 		System.out.println("Unsorted list of students:" + System.lineSeparator() + students);
-		
+
 		try {
 			List<Class<?>> classes = getClasses("ru.yandex.sergaidm");
-			System.out.println(classes);
 			for (Class<?> cl : classes) {
 				if (cl.isAnnotationPresent(Sorter.class)) {
-//					Class<?> cls = Class.forName(cl.getName().substring(cl.getName().lastIndexOf('.')+1));
-					students.sort(cl);
+					try {
+						students.sort((Comparator<Student>) cl.newInstance());
+					} catch (InstantiationException | IllegalAccessException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 			System.out.println("Sorted list of students by annotated class:" + System.lineSeparator() + students);
 		} catch (ClassNotFoundException | IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public static List<Class<?>> getClasses(String pack) throws IOException, ClassNotFoundException {
 		ClassLoader cl = Main.class.getClassLoader();
 		String packageDir = pack.replaceAll("[.]", "/");
