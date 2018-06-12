845c766bc20f399e5be61a6da59c41484127f757
sergaidm <sergaidm@yandex.ru>
2018-06-12 12:00:31 +0300

    3. Servlets. Web-project "Questionnaire"

--- ProgramPRO31/src/main/java/ru/yandex/sergaidm/StatisticServlet.java
+++ ProgramPRO31/src/main/java/ru/yandex/sergaidm/StatisticServlet.java
@@ -13,19 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 public class StatisticServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	private int answerOne=0;
-	private int answerTwo=0;
-	private int answerThree=0;
-	private int answerFour=0;
-	private int answerFive=0;	
-	private int answerSix=0;
-	private int answerSeven=0;
-	private int answerEight=0;
-	private int answerNine=0;
-	private int answerTen=0;
-	private int answerEleven=0;
-	private int answerTwelve=0;
-	private int answerThirteen=0;
+	private int answerOne = 0;
+	private int answerTwo = 0;
+	private int answerThree = 0;
+	private int answerFour = 0;
+	private int answerFive = 0;	
+	private int answerSix = 0;
+	private int answerSeven = 0;
+	private int answerEight = 0;
+	private int answerNine = 0;
+	private int answerTen = 0;
+	private int answerEleven = 0;
+	private int answerTwelve = 0;
+	private int answerThirteen = 0;
 	private static final String TEMPLATE = "<!DOCTYPE html><html>"
 			+ "<head><meta charset=\"UTF-8\"><title>Questionnaire</title></head>"
 			+ "<body><form method=\"get\" action=\"/ProgramPRO31\"><p>%s</p>"
@@ -34,9 +34,9 @@ public class StatisticServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		final String itemOne = request.getParameter("game");
-		final String itemTwo = request.getParameter("literature");
-		final String itemThree = request.getParameter("band");		
+		String itemOne = request.getParameter("game");
+		String itemTwo = request.getParameter("literature");
+		String itemThree = request.getParameter("band");		
 		comparator(itemOne, itemTwo, itemThree);		
 		String result = result();
 		OutputStream os = response.getOutputStream();		
